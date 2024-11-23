e1cd2e6f6f6ad08c1eea9c2d2ad03a473044def1
CvM IGRIS <105749566+Misael-1313@users.noreply.github.com>
2024-11-22 21:44:14 -0300

    Update javascript.js

--- API/javascript.js
+++ API/javascript.js
@@ -1,3 +1,4 @@
+const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json&#39;
 async function vizualizarInformacoesGlobais() {
     const res = await fetch(url)
     const dados = await res.json()
